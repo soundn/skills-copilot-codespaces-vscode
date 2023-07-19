f13fcc28486167d2d3f15910cd50a38d082ac928
soundn <37328347+soundn@users.noreply.github.com>
2023-07-19 12:03:50 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  var skills = document.getElementById("skills");
+  var memberBtn = document.getElementById("memberBtn");
+  var skillsBtn = document.getElementById("skillsBtn");
+  member.style.display = "block";
+  skills.style.display = "none";
+  memberBtn.style.backgroundColor = "#1d1d1d";
+  skillsBtn.style.backgroundColor = "#2d2d2d";
+} 
